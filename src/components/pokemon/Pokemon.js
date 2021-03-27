b3b180b3132d41db02433a1642477567f0be1a61
Frederik Brandt <frederiksbrandt@gmail.com>
2021-03-27 17:57:26 +0100

    worked on the API call

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,11 +1,13 @@
 import styles from "./Pokemon.module.scss";
 
-const Pokemon = ({ image, name, content }) => {
+const Pokemon = ({ image, name, content }, pokemons) => {
   return (
     <>
       <img href='#' src={image}></img>
       <div>
-        <h2>{name}</h2>
+        <h2>
+          <a>{name}</a>
+        </h2>
         <p>{content}</p>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Pokemon from "../components/pokemon/";
 
 import styles from "../styles/Home.module.scss";
 
-export default function Home() {
+export default function Home({ pokemons }) {
+  console.log("pokemon", pokemons);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Pokédex</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
@@ -17,44 +19,38 @@ export default function Home() {
 
         <div className={styles.pokemon}>
           <ul>
-            <li>
-              <Pokemon
-                image='https://via.placeholder.com/150'
-                name='Pikachu'
-                content='This is a yellow pokemon'
-              />
-            </li>
-            <li>
-              <Pokemon
-                image='https://via.placeholder.com/150'
-                name='Pikachu'
-                content='This is a yellow pokemon'
-              />
-            </li>
-            <li>
-              <Pokemon
-                image='https://via.placeholder.com/150'
-                name='Pikachu'
-                content='This is a yellow pokemon'
-              />
-            </li>
-            <li>
-              <Pokemon
-                image='https://via.placeholder.com/150'
-                name='Pikachu'
-                content='This is a yellow pokemon'
-              />
-            </li>
-            <li>
-              <Pokemon
-                image='https://via.placeholder.com/150'
-                name='Pikachu'
-                content='This is a yellow pokemon'
-              />
-            </li>
+            {pokemons.map((pokemon) => {
+              const { image, name, content } = pokemon;
+
+              return (
+                <li>
+                  <Pokemon image={image} name={name} content={content} />
+                </li>
+              );
+            })}
           </ul>
         </div>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+  const { results } = await response.json();
+
+  console.log("json", results);
+
+  const pokemons = results.map((pokemon) => {
+    return {
+      id: pokemon.url,
+      name: pokemon.name,
+    };
+  });
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
 }
